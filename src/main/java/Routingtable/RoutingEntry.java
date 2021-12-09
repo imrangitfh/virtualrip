bc2b72db971749b011878f248ac87f4139f8ad28
Tobias_Koenig <tobias.koenig@stud.fh-campuswien.ac.at>
2021-12-09 11:31:40 +0100

    RoutingEntry & RoutingTable started

--- src/main/java/Routingtable/RoutingEntry.java
+++ src/main/java/Routingtable/RoutingEntry.java
@@ -1,4 +1,18 @@
 package Routingtable;
 
+import Addresses.IP_Address;
+import Addresses.Subnet;
+
 public class RoutingEntry {
+
+    private Subnet network_id;
+    private IP_Address nextHop_IP;
+    private Integer metric;
+
+    public RoutingEntry(Subnet network_id, IP_Address nextHop_IP, Integer metric) {
+        this.network_id = network_id;
+        this.nextHop_IP = nextHop_IP;
+        this.metric = metric;
+    }
+
 }
--- src/main/java/Routingtable/RoutingTable.java
+++ src/main/java/Routingtable/RoutingTable.java
@@ -1,4 +1,19 @@
 package Routingtable;
 
+import java.util.ArrayList;
+
 public class RoutingTable {
+
+    private ArrayList<RoutingEntry> routingtable = new ArrayList<RoutingEntry>();
+
+    public void addRoutingEntry (RoutingEntry routingEntry){
+        //vergleich von versch. Routingentries
+
+        routingtable.add(routingEntry);
+    }
+
+    public ArrayList<RoutingEntry> getRoutingtable() {
+        return routingtable;
+    }
+
 }
