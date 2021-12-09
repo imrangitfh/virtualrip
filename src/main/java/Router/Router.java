118a905765406f51b60aa297e061017c9b37305f
Tobias_Koenig <tobias.koenig@stud.fh-campuswien.ac.at>
2021-12-09 11:20:06 +0100

    addInterface method written + attributes extended

--- src/main/java/Router/Router.java
+++ src/main/java/Router/Router.java
@@ -1,12 +1,28 @@
 package Router;
 
+import Addresses.IP_Address;
+import Addresses.Subnet;
 import Routingtable.RoutingTable;
+
+import java.util.ArrayList;
 
 public class Router {
     private String routerName;
     private RoutingTable routingTable = new RoutingTable();
+    private ArrayList<Interface> Interfaces = new ArrayList<Interface>();
 
     public Router(String routerName) {
         this.routerName = routerName;
     }
+
+
+    public void addInterface (String name, String nr, IP_Address ip_address, Integer snm){
+        Interface new_interface = new Interface(name,nr,ip_address,new Subnet(),this);
+        Interfaces.add(new_interface);
+    }
+
+
+
+
+
 }
